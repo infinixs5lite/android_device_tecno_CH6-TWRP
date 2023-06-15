6bea602b32941b9ce7044f989dc2a7ce54845ffe
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-15 15:29:24 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_TECNO-CH6.mk
+    $(LOCAL_DIR)/twrp_CH6.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_TECNO-CH6-user \
-    omni_TECNO-CH6-userdebug \
-    omni_TECNO-CH6-eng
+    twrp_CH6-user \
+    twrp_CH6-userdebug \
+    twrp_CH6-eng
